32a02ead30f2958b3b0dc0971dd16189a4bf9ea3
Vic <vicdonatti92@gmail.com>
2019-12-09 19:25:02 +0300

    created new features

--- task_tracker/views.py
+++ task_tracker/views.py
@@ -1,15 +1,16 @@
-from django.http import HttpResponseRedirect
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 from task_tracker.models import TaskItem
 
 
-def tasks_view(request):
+def tasks_view(request: WSGIRequest) -> HttpResponse:
     all_task_items = TaskItem.objects.all()
     return render(request, 'index.html', {'all_items': all_task_items})
 
 
-def add_task(request):
+def add_task(request: WSGIRequest) -> HttpResponseRedirect:
     new_item = TaskItem(
         task_name=request.POST['task_name'],
         task_discription=request.POST['task_discription'],
@@ -18,7 +19,7 @@ def add_task(request):
     return HttpResponseRedirect('/')
 
 
-def delete_task(request, task_id):
+def delete_task(request: WSGIRequest, task_id: int) -> HttpResponseRedirect:
     item_to_delete = TaskItem.objects.get(id=task_id)
     item_to_delete.delete()
     return HttpResponseRedirect('/')
